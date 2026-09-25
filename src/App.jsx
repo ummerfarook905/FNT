47e10b31e2a35abd7e69c22911f71b07472d6f87
ummerfarook905 <ummerfarook905@gmail.com>
2026-09-25 22:31:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+   <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
